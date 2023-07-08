a61a053cff6a2406ab9eb81dae58a1e5bfb6cb1f
Тимур <tuhvatullin_163@mail.ru>
2023-07-08 13:53:52 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Привет мир");
         System.out.println("Задача № 1");
         for (int i = 1; i <= 10; i++) {
             System.out.println("Итерация цикла " + i);
